379513c0f838a5e07a02743bcf73507b1ec4cfb8
Gimbol4 <103508859+EndesaptTest@users.noreply.github.com>
2026-02-23 17:40:56 +0300

    Add project files.

---
+++ task01project/logic.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+#define DEFAULT_SIZE 100
+using namespace std;
+
+bool search_linear(int array[DEFAULT_SIZE], int length, int value);
---
+++ task01project/main.cpp
@@ -0,0 +1,26 @@
+#include "util.h"
+
+int main() {
+	int array[DEFAULT_SIZE];
+
+	int length;
+	int a = -20, b = 20;
+	
+	do {
+		cout << "Input size of array: ";
+		cin >> length;
+	} while (length <= 0);
+
+	init_random(array, length, a, b);
+
+	print("Result array:\n");
+	print(convert(array, length));
+
+	int value = 0;
+
+	bool result = search_linear(array, length, value);
+
+	print(result ? "Yes" : "No");
+
+	return 0;
+}
